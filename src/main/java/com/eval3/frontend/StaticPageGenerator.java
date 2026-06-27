dee79454bd232e86d90273df843f5fc695a47ef7
sotom28 <rod.sotod@duocuc.cl>
2026-06-27 00:13:46 -0400

    agregar url de backnend 8

--- src/main/java/com/eval3/frontend/StaticPageGenerator.java
+++ src/main/java/com/eval3/frontend/StaticPageGenerator.java
@@ -11,6 +11,7 @@ public class StaticPageGenerator {
  
     private static final String OUTPUT_DIR = "output";
 
+    
     private static String getEnv(String key, String defaultValue) {
         Path envPath = Paths.get(".env");
         if (Files.exists(envPath)) {
