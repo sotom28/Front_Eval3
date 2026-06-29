37622f1f002dc53c90d34c548277c0dc7303ba04
sotom28 <rod.sotod@duocuc.cl>
2026-06-28 23:01:03 -0400

    agrega  url backend:1

--- src/main/java/com/eval3/frontend/StaticPageGenerator.java
+++ src/main/java/com/eval3/frontend/StaticPageGenerator.java
@@ -8,9 +8,11 @@ import java.nio.file.Paths;
 
 public class StaticPageGenerator {
     
+    
     private static final String OUTPUT_DIR = "output";
 
     
+
     private static String getEnv(String key, String defaultValue) {
         Path envPath = Paths.get(".env");
         if (Files.exists(envPath)) {
